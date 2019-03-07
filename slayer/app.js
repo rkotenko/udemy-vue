41ee542f7436310f1fba6a379a453691b5f78969
rkotenko <robkotenko@gmail.com>
2019-03-07 09:05:20 -0500

    finished slayer game

--- slayer/app.js
+++ slayer/app.js
@@ -1,5 +1,5 @@
 // You may of course also outsource this script code into a separate file (e.g. app.js) and simply import this file here
-new Vue({
+var vm = new Vue({
   el: '#app',
   data: {
     gameStarted: false,
