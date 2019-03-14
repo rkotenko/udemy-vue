e7ef4e15491b7d6c7aafafc4e19cc80919a609a6
Robert G Kotenko (CENSUS/DSD FED) <Robert.G.Kotenko@census.gov>
2019-03-14 09:35:45 -0400

    still working on section 9

---
+++ working/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+              <Quotes>
+                <h1 slot="title">Title</h1>
+                <p slot="content">{{ quote }}</p>
+              </Quotes>  
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  import Quotes from './components/Quotes.vue';
+
+  export default {
+    data() {
+      return {
+        quote: "Inspirational!"
+      }
+    },
+    components: {
+      Quotes
+    }    
+  }
+</script>
+
+<style>
+</style>
---
+++ working/src/components/Quotes.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>
+      <slot name="title"></slot>
+    </h1>
+    <slot name="content"></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid black;
+    margin: 30px auto;
+    box-shadow: 1px 1px 3px 3px;
+    text-align: center;
+    padding: 20px;
+  }
+</style>
